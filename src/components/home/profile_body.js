0cc54aff8ab0e419bd8855af8b6fc601f2f3e711
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-10-25 11:49:48 -0400

    last second tweak

--- src/components/home/profile_body.js
+++ src/components/home/profile_body.js
@@ -13,9 +13,9 @@ const ProfileBody = () => {
           <span className="highlight">application development</span> and{' '}
           <span className="highlight">machine learning</span>, especially where
           the two areas intersect. My current work pertains to the AIM HIGH
-          Laboratory at the Geisel School of Medicine, where I am developing a{' '}
-          <span className="highlight">chatbot</span> using Flutter and ParlAI
-          aimed to help people manage their anxiety and/or depression.
+          Laboratory at the Geisel School of Medicine, where I am developing a
+          chatbot using Flutter and ParlAI aimed to help people manage their
+          anxiety and/or depression.
         </Typography>
       </Grid>
     </Grid>
