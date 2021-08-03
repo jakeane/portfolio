b28a853d7ba25bb5fae2936cd772756810321c88
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-08-03 00:09:31 -0400

    refactor experience components

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -1,54 +1,16 @@
-import React, { useRef, useState } from 'react';
-import Image from 'next/image';
+import React, { useState } from 'react';
 
 import styles from 'styles/Experience.module.css';
 import AngledCorner from 'components/utils/AngledCorner';
 
 import experience from 'public/data/experience.json';
-import dali from 'public/icons/dali.svg';
-import bofa from 'public/icons/bofa.svg';
-import dartmouth from 'public/icons/dartmouth.svg';
-import geisel from 'public/icons/geisel.svg';
-import asc from 'public/icons/asc.svg';
-import daliBlack from 'public/icons/dali_black.svg';
-import bofaBlack from 'public/icons/bofa_black.svg';
-import dartmouthBlack from 'public/icons/dartmouth_black.svg';
-import geiselBlack from 'public/icons/geisel_black.svg';
-import ascBlack from 'public/icons/asc_black.svg';
-import x from 'public/icons/x.svg';
-import rightArrow from 'public/icons/right_arrow.svg';
 import { ExperienceItem } from 'types/json';
-
-const LOGOS: Record<string, any> = {
-  dali,
-  bofa,
-  dartmouth,
-  geisel,
-  asc,
-};
-
-const BLACK_LOGOS: Record<string, any> = {
-  dali: daliBlack,
-  bofa: bofaBlack,
-  dartmouth: dartmouthBlack,
-  geisel: geiselBlack,
-  asc: ascBlack,
-};
+import ExperienceCard from 'components/subcomponents/ExperienceCard';
+import ExperienceModal from 'components/subcomponents/ExperienceModal';
 
 const Experience: React.ForwardRefRenderFunction<HTMLDivElement> = (_props, ref) => {
   const [modalData, setModalData] = useState<ExperienceItem>(experience[0]);
   const [showModal, setShowModal] = useState(false);
-  const modalRef = useRef<HTMLDivElement>(null);
-
-  const handleExitClick = () => {
-    document.body.style.overflow = 'unset';
-    setShowModal(false);
-  };
-
-  const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (modalRef.current?.contains(e.target as Node)) return;
-    handleExitClick();
-  };
 
   const handleLearnMore = (e: ExperienceItem) => {
     document.body.style.overflow = 'hidden';
@@ -59,36 +21,19 @@ const Experience: React.ForwardRefRenderFunction<HTMLDivElement> = (_props, ref)
   return (
     <div className={styles.main} ref={ref}>
       {showModal && (
-        <div className={styles.modal_background} onClick={handleOutsideClick}>
-          <div className={styles.modal} ref={modalRef}>
-            <div className={styles.x} onClick={handleExitClick}>
-              <Image src={x} alt='x' />
-            </div>
-            <div className={styles.modal_headers}>
-              <Image src={BLACK_LOGOS[modalData.logo]} alt={modalData.logo} />
-              <h3 className={styles.modal_position}>{modalData.position}</h3>
-              <p className={styles.modal_time}>{modalData.start} - {modalData.end}</p>
-            </div>
-            <div className={styles.modal_description}>
-              <p>{modalData.description}</p>
-            </div>
-          </div>
-        </div>
+        <ExperienceModal
+          experience={modalData}
+          setShowModal={setShowModal}
+        />
       )}
       <AngledCorner width={60} height={100}>
         <div className={styles.container}>
           {experience.map((e) => (
-            <div className={styles.card} key={`${e.company}-${e.position}`}>
-              <div className={styles.img}>
-                <Image src={LOGOS[e.logo]} alt={e.logo} />
-              </div>
-              <h3 className={styles.position}>{e.position}</h3>
-              <p className={styles.time}>{e.start} - {e.end}</p>
-              <div className={styles.learn_more} onClick={() => handleLearnMore(e)}>
-                <p className={styles.lm_text}>Learn more</p>
-                <Image src={rightArrow} alt='arrow' />
-              </div>
-            </div>
+            <ExperienceCard
+              key={`${e.company}-${e.position}`}
+              experience={e}
+              handleLearnMore={handleLearnMore}
+            />
           ))}
         </div>
       </AngledCorner>
---
+++ components/subcomponents/ExperienceModal.tsx
@@ -0,0 +1,58 @@
+import React, { useRef } from 'react';
+import Image from 'next/image';
+
+import daliBlack from 'public/icons/dali_black.svg';
+import bofaBlack from 'public/icons/bofa_black.svg';
+import dartmouthBlack from 'public/icons/dartmouth_black.svg';
+import geiselBlack from 'public/icons/geisel_black.svg';
+import ascBlack from 'public/icons/asc_black.svg';
+import x from 'public/icons/x.svg';
+import styles from 'styles/Experience.module.css';
+import { ExperienceItem } from 'types/json';
+
+const BLACK_LOGOS: Record<string, any> = {
+  dali: daliBlack,
+  bofa: bofaBlack,
+  dartmouth: dartmouthBlack,
+  geisel: geiselBlack,
+  asc: ascBlack,
+};
+
+interface Props {
+  experience: ExperienceItem
+  setShowModal: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const ExperienceModal: React.FC<Props> = ({ experience, setShowModal }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const handleExitClick = () => {
+    document.body.style.overflow = 'unset';
+    setShowModal(false);
+  };
+
+  const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (modalRef.current?.contains(e.target as Node)) return;
+    handleExitClick();
+  };
+
+  return (
+    <div className={styles.modal_background} onClick={handleOutsideClick}>
+      <div className={styles.modal} ref={modalRef}>
+        <div className={styles.x} onClick={handleExitClick}>
+          <Image src={x} alt='x' />
+        </div>
+        <div className={styles.modal_headers}>
+          <Image src={BLACK_LOGOS[experience.logo]} alt={experience.logo} />
+          <h3 className={styles.modal_position}>{experience.position}</h3>
+          <p className={styles.modal_time}>{experience.start} - {experience.end}</p>
+        </div>
+        <div className={styles.modal_description}>
+          <p>{experience.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceModal;
