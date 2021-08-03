5b855f5173f4ac4a4df28dc876c11f2203eb7a85
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-08-02 22:01:49 -0400

    create base about me

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -10,8 +10,15 @@ const About: React.ForwardRefRenderFunction<HTMLDivElement> = (_props, ref) => (
   <div className={styles.main} ref={ref}>
     <AngledCorner right width={55} height={60}>
       <div className={styles.container}>
-        <div className={styles.img}>
-          <Image src={profile} alt='profile' />
+        <div className={styles.img_container}>
+          <Image src={profile} alt='profile' className={styles.img} />
+        </div>
+        <div className={styles.text_container}>
+          <h2 className={styles.title}>About me</h2>
+          <p className={styles.body}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus semper odio, sit amet consequat arcu accumsan tincidunt. Etiam eu elit in arcu feugiat convallis.
+          </p>
+          <p className={styles.body}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac turpis diam. Quisque iaculis sed.</p>
         </div>
       </div>
     </AngledCorner>
