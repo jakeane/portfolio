907ead6a00c2c6e5a31999b3d388bce49124c9e0
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-08-21 00:58:13 -0400

    change linkedin url

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,4 @@
 export const GITHUB = 'https://www.github.com/jakeane';
-export const LINKEDIN = 'https://www.linkedin.com/in/jack-keane-co/';
+export const LINKEDIN = 'https://www.linkedin.com/in/jack-keane-v/';
 export const EMAIL = 'john.f.keane.22@dartmouth.edu';
 export const SECTIONS = ['Home', 'Experience', 'Projects', 'About', 'Links'];
